919d292bef5122b161960eea3c91aba22ae492df
wishajaveria <49516832+wishajaveria@users.noreply.github.com>
2019-07-11 12:09:55 +0500

    wip

---
+++ script.js
@@ -0,0 +1,9 @@
+function print(){
+    //let students = [{name:"areeba",contact:"03333257810",message:"I am ok"},/*{name:"minhal",contact:"03323427891",message:"see you tomorrow"},{name:"anika",contact:"03225680067",message:"where you are?"},{name:"samreen",contact:"0336788895",message:"thank for book"},{name:"rabia",contact:"07344567282",message:"I am not feeling well"}*/];
+    //onsole.log(students);
+    //let areeba = ["anika" , "o333252678" , "hello"];
+
+
+    let students = [{name:"areeba" , contact:"0334562673" , message:"hello"} , {name:"anika" ,contact: "02253674894", message:"ok"} , {name:"ayesha" , contact:"03649439202" ,message: "well!!"}];
+    console.log(students);
+}
